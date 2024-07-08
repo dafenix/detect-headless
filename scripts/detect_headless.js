0c3d7e77c5ffb02640998e0f67ea523141221d13
Daniel Weber <daniel.weber@external.merckgroup.com>
2024-07-08 14:32:45 +0200

    Add font detection

--- scripts/detect_headless.js
+++ scripts/detect_headless.js
@@ -281,6 +281,144 @@ function testMouseMove(resultBlock) {
   }
 }
 
+function testFonts(resultBlock) {
+  /**
+   * JavaScript code to detect available availability of a
+   * particular font in a browser using JavaScript and CSS.
+   *
+   * Author : Lalit Patel
+   * Website: http://www.lalit.org/lab/javascript-css-font-detect/
+   * License: Apache Software License 2.0
+   *          http://www.apache.org/licenses/LICENSE-2.0
+   * Version: 0.15 (21 Sep 2009)
+   *          Changed comparision font to default from sans-default-default,
+   *          as in FF3.0 font of child element didn't fallback
+   *          to parent element if the font is missing.
+   * Version: 0.2 (04 Mar 2012)
+   *          Comparing font against all the 3 generic font families ie,
+   *          'monospace', 'sans-serif' and 'sans'. If it doesn't match all 3
+   *          then that font is 100% not available in the system
+   * Version: 0.3 (24 Mar 2012)
+   *          Replaced sans with serif in the list of baseFonts
+   */
+
+  /**
+   * Usage: d = new Detector();
+   *        d.detect('font name');
+   */
+  var Detector = function() {
+  // a font will be compared against all the three default fonts.
+  // and if it doesn't match all 3 then that font is not available.
+  var baseFonts = ['monospace', 'sans-serif', 'serif'];
+
+  //we use m or w because these two characters take up the maximum width.
+  // And we use a LLi so that the same matching fonts can get separated
+  var testString = "mmmmmmmmmmlli";
+
+  //we test using 72px font size, we may use any size. I guess larger the better.
+  var testSize = '72px';
+
+  var h = document.getElementsByTagName("body")[0];
+
+  // create a SPAN in the document to get the width of the text we use to test
+  var s = document.createElement("span");
+  s.style.fontSize = testSize;
+  s.innerHTML = testString;
+  var defaultWidth = {};
+  var defaultHeight = {};
+  for (var index in baseFonts) {
+      //get the default width for the three base fonts
+      s.style.fontFamily = baseFonts[index];
+      h.appendChild(s);
+      defaultWidth[baseFonts[index]] = s.offsetWidth; //width for the default font
+      defaultHeight[baseFonts[index]] = s.offsetHeight; //height for the defualt font
+      h.removeChild(s);
+  }
+
+  function detect(font) {
+      var detected = false;
+      for (var index in baseFonts) {
+          s.style.fontFamily = font + ',' + baseFonts[index]; // name of the font along with the base font for fallback.
+          h.appendChild(s);
+          var matched = (s.offsetWidth != defaultWidth[baseFonts[index]] || s.offsetHeight != defaultHeight[baseFonts[index]]);
+          h.removeChild(s);
+          detected = detected || matched;
+      }
+      return detected;
+  }
+
+  this.detect = detect;
+};
+  var fontList = [
+    // This is android-specific font from "Roboto" family
+    'sans-serif-thin',
+    'ARNO PRO',
+    'Agency FB',
+    'Arabic Typesetting',
+    'Arial Unicode MS',
+    'AvantGarde Bk BT',
+    'BankGothic Md BT',
+    'Batang',
+    'Bitstream Vera Sans Mono',
+    'Calibri',
+    'Century',
+    'Century Gothic',
+    'Clarendon',
+    'EUROSTILE',
+    'Franklin Gothic',
+    'Futura Bk BT',
+    'Futura Md BT',
+    'GOTHAM',
+    'Gill Sans',
+    'HELV',
+    'Haettenschweiler',
+    'Helvetica Neue',
+    'Humanst521 BT',
+    'Leelawadee',
+    'Letter Gothic',
+    'Levenim MT',
+    'Lucida Bright',
+    'Lucida Sans',
+    'Menlo',
+    'MS Mincho',
+    'MS Outlook',
+    'MS Reference Specialty',
+    'MS UI Gothic',
+    'MT Extra',
+    'MYRIAD PRO',
+    'Marlett',
+    'Meiryo UI',
+    'Microsoft Uighur',
+    'Minion Pro',
+    'Monotype Corsiva',
+    'PMingLiU',
+    'Pristina',
+    'SCRIPTINA',
+    'Segoe UI Light',
+    'Serifa',
+    'SimHei',
+    'Small Fonts',
+    'Staccato222 BT',
+    'TRAJAN PRO',
+    'Univers CE 55 Medium',
+    'Vrinda',
+    'ZWAdobeF',
+  ];
+  var detective = new Detector();
+  //for (const font of fontList.filter(p => detective.detect(p) === true)) { console.log(''+font); }
+  var matchedFonts = [];
+  for(var i = 0; i < fontList.length; i++) {
+    if(detective.detect(fontList[i])) {
+      matchedFonts.push(fontList[i]);
+    }
+  }
+  writeToBlock(resultBlock, `Detected fonts: ${matchedFonts.join(', ')}`);
+  return HEADFUL;
+}
+
+
+
+
 function mouseMoveWriteResult(resultBlock, zeroMovement) {
   if (zeroMovement)
     writeToBlock(resultBlock, "MovementX and movementY are 0 in every mouse event");
@@ -300,7 +438,7 @@ const tests = [
   { name: "Mime Prototype",    id: "mime-prototype",    testFunction: testMimePrototype    },
   { name: "Languages",         id: "languages",         testFunction: testLanguages        },
   { name: "Webdriver",         id: "webdriver",         testFunction: testWebdriver        },
-  { name: "Time Elapse",       id: "time-elapse",       testFunction: testTimeElapse       },
+  //{ name: "Time Elapse",       id: "time-elapse",       testFunction: testTimeElapse       },
   { name: "Chrome",            id: "chrome-element",    testFunction: testChrome           },
   { name: "Permission",        id: "permission",        testFunction: testPermission       },
   { name: "Devtool Protocol",  id: "devtool",           testFunction: testDevtool          },
@@ -308,6 +446,7 @@ const tests = [
   { name: "Outer dimensions",  id: "outer",             testFunction: testOuter            },
   { name: "Connection Rtt",    id: "connection-rtt",    testFunction: testConnectionRtt     },
   { name: "Mouse Move",        id: "mouse-move",        testFunction: testMouseMove        },
+  { name: "Find Fonts",        id: "find-fonts",        testFunction: testFonts        },
 ];
 
 tests.forEach(test => {
